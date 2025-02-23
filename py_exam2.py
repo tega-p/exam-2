793936372ae422c519f4e4ea9364214ac80e1b26
tega-p <ppries01@student.bbk.ac.uk>
2025-02-23 09:12:23 +0000

    Update py_exam2.py

--- py_exam2.py
+++ py_exam2.py
@@ -52,6 +52,8 @@ def top_lysine_stats(filename: str = "multi_seqs.txt") -> Tuple[float, str]:
 
     for seq in sequences:
         lysine_count = seq.count('K')
+        if len(seq)  == 0:
+            continue
         percentage = (lysine_count / len (seq)) * 100 if seq else 0
      
         if percentage > max_percentage:
